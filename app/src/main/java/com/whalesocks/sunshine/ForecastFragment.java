062f3886648fea0ba49d1128ce3e20ca215631c1
bennychee <chee.benny@gmail.com>
2015-06-16 12:43:36 +0800

    16/6/2015 Commit from Office

--- app/src/main/java/com/whalesocks/sunshine/ForecastFragment.java
+++ app/src/main/java/com/whalesocks/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.whalesocks.sunshine;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -89,6 +92,17 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+              String forecast = mForecastAdapter.getItem(position);
+                Toast.makeText(
+                      getActivity(),
+                      forecast,
+                      Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         return rootView;
     }
