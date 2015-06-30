1e4c7f0a7cbc0e49db4dc21b821413838c24c2df
bennychee <chee.benny@gmail.com>
2015-06-30 16:23:20 +0800

    30/6/2015 Commit from Office

--- app/src/main/java/com/whalesocks/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/whalesocks/sunshine/data/WeatherContract.java
@@ -44,7 +44,7 @@ public class WeatherContract {
         public static final String COLUMN_LOCATION_SETTING= "location_setting";
         public static final String COLUMN_CITY_NAME = "city_name";
         public static final String COLUMN_COORD_LAT = "coor_lat";
-        public static final String COLUMN_COOR_LONG = "coor_long";
+        public static final String COLUMN_COORD_LONG = "coor_long";
 
     }
 
